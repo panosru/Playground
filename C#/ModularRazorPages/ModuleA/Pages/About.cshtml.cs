e7d21196d2ad3208df3d9f643160d2cc704865ba
Panagiotis <400362+panosru@users.noreply.github.com>
2020-12-27 12:36:17 +0200

    ModularRazorPages experiment

---
+++ C#/ModularRazorPages/ModuleA/Pages/About.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ModuleA.Pages
+{
+    public class About : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
